250231a0d513bee6a25106be6a83845500cd202f
a.22308051281092 <a.22308051281092@cbtis128.edu.mx>
2025-05-29 15:56:31 +0000

    agregue Avatar

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -43,7 +43,7 @@ class HomePage extends StatelessWidget {
               currentAccountPicture: GestureDetector( 
                 child: CircleAvatar(
                   child: Text(
-                    "AM",
+                    "AR",
                     style: TextStyle(
                         color: Colors.pink,
                         fontSize: 20.0,
